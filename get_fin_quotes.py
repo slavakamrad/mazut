f023c89ea1a7442a562f1a85438e236ed126dff0
Slava Kamrad <slavakamrad@gmail.com>
2020-11-15 23:58:59 +0300

    Update get_fin_quotes.py

--- get_fin_quotes.py
+++ get_fin_quotes.py
@@ -2,7 +2,7 @@ from alpha_vantage.timeseries import TimeSeries
 
 
 def get_fin_quotes(ticker_symbol):
-    ts = TimeSeries(key="QG9CQILAD7ASQXK0")
+    ts = TimeSeries(key="")
     data = ts.get_intraday(ticker_symbol, interval='1min')
     return str(data[0][max(data[0].keys())])
 
