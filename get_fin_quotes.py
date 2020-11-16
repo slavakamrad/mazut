e4562cee4b5773b2fe01e5a465d9c7be754fd849
Slava <slavakamrad@gmail.com>
2020-11-16 19:59:08 +0300

    update get_fin_quotes.py

--- get_fin_quotes.py
+++ get_fin_quotes.py
@@ -5,8 +5,11 @@ from alpha_vantage.foreignexchange import ForeignExchange
 def get_fin_quotes(ticker_symbol):
     ts = TimeSeries(key="QG9CQILAD7ASQXK0")
     data = ts.get_quote_endpoint(ticker_symbol)
+    symbol_search = ts.get_symbol_search(ticker_symbol)
     try:
-        return ticker_symbol + ": Цена - " + str(data[0]['05. price']) + " Объем - " + str(data[0]['06. volume']) \
+        return str(symbol_search[0][0]['2. name']) \
+               + ": Цена - " + str(data[0]['05. price']) \
+               + " Объем - " + str(data[0]['06. volume']) \
                + " Процент изменений с последнего торгового дня - " + str(data[0]['10. change percent'])
     except ValueError:
         print("Неверный тикет")
@@ -24,11 +27,3 @@ def exchange_rate_euro():
     ts = ForeignExchange(key='BK1STKHPQSJVPL20')
     data = ts.get_currency_exchange_rate(from_currency='USD', to_currency='RUB')
     return str(round(float(data[0]['5. Exchange Rate']), 2)) + "р."
-
-
-
-
-
-
-
-
